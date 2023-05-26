84f2d8843e1e50850441ad8e40fbc5bf40a6b1ca
Ethan Sternke <Ethan.Sternke@litera.com>
2023-05-26 14:58:26 -0400

    payments index

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'contacts#index'
 
   get '/contacts', to: 'contacts#index'
+  get '/payments', to: 'payments#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
