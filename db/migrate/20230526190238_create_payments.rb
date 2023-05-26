34c1240d610bff08a1b704afe8e1facfe37ff870
Ethan Sternke <Ethan.Sternke@litera.com>
2023-05-26 15:03:18 -0400

    payment model

---
+++ db/migrate/20230526190238_create_payments.rb
@@ -0,0 +1,13 @@
+class CreatePayments < ActiveRecord::Migration[7.0]
+  def change
+    create_table :payments do |t|
+      t.integer :advanced_amount
+      t.datetime :payment_date
+      t.string :deducted_from_bl
+      t.integer :amount_deducted
+      t.integer :total_rebate
+
+      t.timestamps
+    end
+  end
+end
