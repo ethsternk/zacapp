22aeb9771f9cba61eb00df7cef793933489cc7fe
Ethan Sternke <Ethan.Sternke@litera.com>
2023-06-30 20:00:35 -0400

    Create new contact form

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,4 +7,23 @@ class ContactsController < ApplicationController
     @contact = Contact.find(params[:id])
     @payments = @contact.payments
   end
+
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+
+    if @contact.save
+      redirect_to @contact
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def contact_params
+      params.require(:contact).permit(:first_name, :last_name, :address, :city, :state_or_province, :postal_code, :home_phone, :mobile_phone, :notes, :paid_in_full, :email)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'contacts#index'
 
   resources :contacts
-  get '/payments', to: 'payments#index'
+  resources :payments
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
